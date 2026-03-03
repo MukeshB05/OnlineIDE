6da29caa7b01f9002573991a7e7a498f271ac3a7
Mukesh <mukesh2305b@gmail.com>
2026-03-03 22:15:47 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,54 @@
+"use client";
+import Editor from "@/components/Editor";
+import MobileEditor from "@/components/MobileEditor";
+import useMediaQuery from '../hooks/useMediaQuery';
+import { Code2, Loader2 } from "lucide-react";
+import { useState, useEffect, Fragment, createContext } from "react";
+import { download } from "@/lib/download";
+
+export const CodeContext = createContext(null);
+export default function Page() {
+  const screenSize = useMediaQuery("768");
+  // const screenSize = useMediaQuery("300");
+  const [preload, setPreload] = useState(true);
+  const [codes, setCodes] = useState(null);
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+
+  useEffect(() => {
+    setPreload(false);
+    const preventDefaultSave = (e) => {
+      if ((e.key === "s" && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) || e.button === 2) {
+        e.preventDefault();
+      }
+    };
+    document.addEventListener("keydown", preventDefaultSave, false);
+    document.addEventListener("contextmenu", preventDefaultSave, false);
+    return () => {
+      document.removeEventListener("keydown", preventDefaultSave, false);
+      document.removeEventListener("contextmenu", preventDefaultSave, false);
+    };
+
+  }, []);
+
+  useEffect(() => {
+    setHtml(localStorage.getItem("html") || "");
+    setCss(localStorage.getItem("css") || "");
+    setJs(localStorage.getItem("js") || "");
+  },[]);
+
+  return (
+    <>
+      {preload && (
+        <div className="absolute h-full w-full z-[99999] bg-background flex items-center justify-center">
+          <Loader2 className="h-6 w-6 animate-spin" />
+        </div>
+      )}
+      < CodeContext.Provider value={{ codes, setCodes, html, setHtml, css, setCss, js, setJs }
+      }>
+        {!screenSize ? <Editor /> : <MobileEditor />}
+      </CodeContext.Provider >
+    </>
+  )
+};
