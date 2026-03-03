a9ece380d741babe3242c5ba8576bb60e03cfcf7
Mukesh <mukesh2305b@gmail.com>
2026-03-03 22:51:52 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer({ isLoading = false, runCode = () => { }, onClear
         <footer className="py-2 px-2 sm:px-6 flex items-center gap-2 justify-between">
             <div className="flex items-center gap-[6px]">
                 <Button size="icon" disabled={isLoading} onClick={runCode}>{isLoading ? <Loader2 className="animate-spin h-3.5 w-3.5" /> : <Play className="h-3.5 w-3.5" />}</Button>
-                {/* <Button size="icon" variant="secondary"><a target="_blank" href="https://github.com/r2hu1/ide/fork"><GitFork className="h-3.5 w-3.5" /></a></Button> */}
+                {/* <Button size="icon" variant="secondary"><a target="_blank" href="https://github.com/MukeshB05/ide/fork"><GitFork className="h-3.5 w-3.5" /></a></Button> */}
             </div>
             <div className="flex items-center gap-[6px]">
                 <Button size="sm" variant="secondary" onClick={onClear}>Clear</Button>
